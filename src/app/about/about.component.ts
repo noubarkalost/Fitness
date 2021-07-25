c61e5a43372294130c8be4bf5fe4fe8b617726fd
noubar <noubar.kaloust@gmail.com>
2021-07-25 22:55:42 +0400

    refactoring

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  aboutRead: string = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using "
   constructor() { }
 
   ngOnInit(): void {
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  aboutRead : string = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using "
   constructor() { }
 
   ngOnInit(): void {
