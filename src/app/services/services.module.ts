be7740115a3087a0b5d084a5f18152dd6ad1966a
noubar <noubar.kaloust@gmail.com>
2021-07-25 19:19:48 +0400

    news rout and design

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ServicesRoutingModule } from './services-routing.module';
 import { ServicesComponent } from './services.component';
+import { ServicesHeaderComponent } from './services-header/services-header.component';
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
   declarations: [
-    ServicesComponent
+    ServicesComponent,
+    ServicesHeaderComponent
   ],
-  imports: [
-    CommonModule,
-    ServicesRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        ServicesRoutingModule,
+        SharedModule
+    ]
 })
 export class ServicesModule { }
